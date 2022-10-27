ecfc74c56146e2b6e792ad172e32bb0e899483a9
Jxstxs <juleb0202@gmail.com>
2022-10-28 00:25:59 +0200

    feat: added function prototypes

--- lua/conceal/init.lua
+++ lua/conceal/init.lua
@@ -4,3 +4,17 @@ if not status then
     print("[conceal.nvim] ERROR: couldnt require treesitter!")
     return
 end
+
+local M = {}
+--- Sets up the wanted languages
+---@param opts table user specific values
+M.setup = function(opts)
+end
+--- Toggles the State of the Conceal Level
+---@param level integer to which level it should be set (could be nil, then it uses the pre defined value)
+M.toggle_conceal = function(level)
+end
+--- generates the treesitter files
+M.generate_conceals = function()
+end
+return M
