c39e5b5f725e2836b361b489027eb66ee6aa3f22
Jxstxs <juleb0202@gmail.com>
2022-10-29 23:14:58 +0200

    feat: added some test lua queries

--- lua/conceal/config.lua
+++ lua/conceal/config.lua
@@ -10,10 +10,72 @@ return {
 
     ["lua"] = {
         enabled = true,
-        ["require"] = {
-            enabled = true,
-            conceal = "rEq",
-            highlight = "TSNote"
-        },
+        keywords = {
+            ["require"] = {
+                enabled = true,
+                conceal = "r",
+                highlight = "function.builtin"
+            },
+            ["local"] = {
+                enabled = true,
+                conceal = "l",
+                highlight = "keyword"
+            },
+            ["function"] = {
+                enabled = true,
+                conceal = "f",
+                highlight = "keyword.function"
+            },
+            ["if"] = {
+                enabled = true,
+                conceal = "?",
+                highlight = "conditional"
+            },
+            ["else"] = {
+                enabled = true,
+                conceal = "e",
+                highlight = "conditional"
+            },
+            ["elseif"] = {
+                enabled = true,
+                conceal = "e",
+                highlight = "conditional"
+            },
+            ["for"] = {
+                enabled = true,
+                conceal = "F",
+                highlight = "repeat"
+            },
+            ["return"] = {
+                enabled = true,
+                conceal = "R",
+                highlight = "keyword.function"
+            },
+            ["and"] = {
+                enabled = true,
+                conceal = "&",
+                highlight = "keyword.function"
+            },
+            ["end"] = {
+                enabled = true,
+                conceal = "E",
+                highlight = "keyword.function"
+            },
+            ["then"] = {
+                enabled = true,
+                conceal = "t",
+                highlight = "conditional"
+            },
+            ["do"] = {
+                enabled = true,
+                conceal = "d",
+                highlight = "repeat"
+            },
+            ["in"] = {
+                enabled = true,
+                conceal = "i",
+                highlight = "keyword"
+            },
+        }
     }
 }
